71faf6445c0fc34a029e4ee6c5c4134d6a48adc7
citizen404 <nismorus@gmail.com>
2024-05-28 14:38:02 +0400

    close button

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     }
     return (
         <div className="App">
-            <button onClick={{onClose}}>Close</button>
+            <button onClick={onClose}>Close</button>
         </div>
     );
 }
