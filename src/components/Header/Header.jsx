fdbb660201bebf6c61703a240c27a11704782bfe
citizen404 <nismorus@gmail.com>
2024-05-28 16:00:59 +0400

    head and buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,12 @@ import {useTelegram} from "../../hooks/useTelegram"
 const Header = () => {
     const {user, onClose} = useTelegram()
     return (
-        <div className = {'header'}>
+        <div className={'header'}>
             <Button onClick={onClose}>Close</Button>
             <span className={'username'}>
                 {user?.username}
             </span>
-
         </div>
     )
 }
+export default Header;
